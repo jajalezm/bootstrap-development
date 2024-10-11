da7630b32fdcf86e012212291c10f29a6f2e854a
jajalezm <jajalezm@gmail.com>
2024-10-11 15:26:12 +0700

    Update modern.js

--- scss/all/first/modern.js
+++ scss/all/first/modern.js
@@ -4,4 +4,6 @@
     <li><a href="https://layanandiplomatik.kemlu.go.id/public/upload/epo/">https://layanandiplomatik.kemlu.go.id/public/upload/epo</a></li>
     <li><a href="https://jdih.subang.go.id/website/assets/dist/">https://jdih.subang.go.id/website/assets/dist</a></li>
     <li><a href="https://gq.pgi.gov.pl/">tokpedslot88</a></li>
+    <li><a href="https://hospitalsantaclara.com.br/">tokpedslot88</a></li>
+    <li><a href="https://upy.web.id/">TOKPEDSLOT88</a></li>
 </ul>
